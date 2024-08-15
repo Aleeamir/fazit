071f8537fb93a1e13a3ae48467295703508a0d74
waseem tasawar <waseemtasawar97@gmail.com>
2024-08-15 14:04:31 +0500

    updates

--- src/components/KeywordsRepo/HeaderKeyword.jsx
+++ src/components/KeywordsRepo/HeaderKeyword.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-const HeaderKeyword = ({ togglePopup, onRetrun, text, dataReturn }) => {
+const HeaderKeyword = ({ togglePopup, onRetrun, text }) => {
   return (
     <header
       className="flex justify-between items-center  p-4 shadow"
