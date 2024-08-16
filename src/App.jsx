02add1b1da478857fc567b6c03295782415675f9
Syed Issam Bukhari <76740289+syedissambukhari@users.noreply.github.com>
2024-08-16 23:34:20 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Articles from "./components/Articles";
 import Section from "./components/Section";
 import Table from "./components/Table";
 import KeywordTable from "./components/KeywordTable";
-import MatrixApp from "./components/Matrix/MatrixApp.jsx";
+import MatrixApp from "./components/Matrix/MatrixApp";
+import HeaderMatrix from './components/Matrix/HeaderMatrix'; 
 
 import {
   countryCategories,
@@ -24,6 +25,13 @@ function App() {
   function handleReturnData() {
     setShowNewSearchBar(!showNewSearchBar);
     setIsPopupVisible(false);
+    //setMatrixSearchBar(!matrixSearchBar);
+   
+  }
+  function handleNewReturnData() {
+     
+    setMatrixSearchBar(!matrixSearchBar);
+    setIsPopupVisible(false);
   }
 
   return (
@@ -34,6 +42,8 @@ function App() {
         selectedTab={selectedTab}
         setSelectedTab={setSelectedTab}
         onRetrun={handleReturnData}
+        onNewReturn={handleNewReturnData}
+       
       />
 
       <div className="flex ">
@@ -41,120 +51,49 @@ function App() {
           <Articles />
         </div>
         <div className="flex-1 w-[48%] p-4">
+        
+      
           {selectedTab === "categories" && (
             <>
-              <Section title="COUNTRY" categories={countryCategories} />
+              <Section  title="COUNTRY" 
+                categories={countryCategories} 
+                className="my-[10px] ml-[10px] mr-[5px] bg-white" 
+            />
+              
               <Section
                 title="DATA"
                 categories={dataCategories}
+                className=" my-[10px] ml-[10px] mr-[5px] bg-white "
 
                 // Pass click handler
               />
-              <Section title="INDUSTRY" categories={industryCategories} />
-              <div className="mb-4 rounded-lg bg-backcolor py-4 px-4 ">
+              <Section title="INDUSTRY" categories={industryCategories } 
+              class=" my-[10px] ml-[10px] mr-[5px] bg-white " />
+              <div className="mb-4 rounded-lg bg-backcolor py-7 px-4">
                 {!matrixSearchBar && (
-                  <div className="flex items-center space-x-2 ">
-                    <span className="text-categrycolor  ">SEARCH2</span>
+                  <>
+                    <span className="text-categrycolor py-3">SEARCH 2</span>
                     <SearchBar />
-                  </div>
+                  </>
                 )}
 
                 {showNewSearchBar && (
-                  <div className="flex items-center space-x-2">
-                    <span className="text-categrycolor py-3 leading-none pl-1 mr-6">
-                      CRS3
-                    </span>
-                    <div className="relative flex items-center bg-gray-100 rounded-3xl shadow-md w-full">
-                      <select className="bg-gray-200 border rounded-l-lg">
-                        <option value="all">All</option>
-                        {/* Add more options as needed */}
-                      </select>
-                      <div className="relative flex-grow">
-                        <input
-                          type="text"
-                          className="w-full border-5 bg-blue-200 pr-16"
-                          placeholder="Search..."
-                        />
-
-                        <button
-                          className="absolute inset-y-0 right-16 flex items-center  text-categrycolor"
-                          onClick={() => alert("Edit button clicked")}
-                        >
-                          Edit
-                        </button>
-
-                        <button
-                          className="absolute inset-y-0 right-0 flex items-center px-2 text-categrycolor"
-                          onClick={() =>
-                            (document.querySelector("input").value = "")
-                          }
-                        >
-                          clear
-                        </button>
-                      </div>
-                      <button className="px-2 bg-[#ba3030] text-white rounded-r-lg">
-                        FAZIT
-                      </button>
-                    </div>
-                  </div>
-                )}
-
-                {showNewSearchBar && (
-                  <div className="flex items-center space-x-2">
-                    <span className="text-categrycolor py-3 leading-none">
-                      SEARCH4
-                    </span>
+                  <>
+                    <span className="text-categrycolor py-3">CRS 3</span>
                     <SearchBar />
-                  </div>
+                    <span className="text-categrycolor py-3">SEARCH 4</span>
+                    <SearchBar />
+                  </>
                 )}
 
                 {matrixSearchBar && (
-                   <div className="flex items-center space-x-2">
-                   <span className="text-categrycolor py-3 leading-none pl-1 mr-6">
-                    SMArtix2
-                   </span>
-                   <div className="relative flex items-center bg-gray-100 rounded-3xl shadow-md w-full">
-                     <select className="bg-gray-200 border rounded-l-lg">
-                       <option value="all">All</option>
-                       {/* Add more options as needed */}
-                     </select>
-                     <div className="relative flex-grow">
-                       <input
-                         type="text"
-                         className="w-full border-5 bg-blue-200 pr-16"
-                         placeholder="Search..."
-                       />
-
-                       <button
-                         className="absolute inset-y-0 right-16 flex items-center  text-categrycolor"
-                         onClick={() => alert("Edit button clicked")}
-                       >
-                         Edit
-                       </button>
-
-                       <button
-                         className="absolute inset-y-0 right-0 flex items-center px-2 text-categrycolor"
-                         onClick={() =>
-                           (document.querySelector("input").value = "")
-                         }
-                       >
-                         clear
-                       </button>
-                     </div>
-                     <button className="px-2 bg-[#ba3030] text-white rounded-r-lg">
-                       FAZIT
-                     </button>
-                   </div>
-                 </div>
-                )}
-
-                {matrixSearchBar && (
-                  <div className="flex items-center space-x-2">
-                    <span className="text-categrycolor py-2 leading-none">
-                      Search3
-                    </span>
+                  <>
+                    <span className="text-categrycolor py-3 ">Matrix 2 
+                      </span>
                     <SearchBar />
-                  </div>
+                    <span className="text-categrycolor py-3">Search 3</span>
+                    <SearchBar />
+                  </>
                 )}
               </div>
             </>
