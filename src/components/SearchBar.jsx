447aa505da32388c11fa30890fc4fe0ee54a5342
Syed Issam Bukhari <76740289+syedissambukhari@users.noreply.github.com>
2024-08-17 02:05:24 +0500

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,30 +3,20 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="relative flex items-center bg-gray-100 rounded-3xl pl-0 mr-0 shadow-md w-full">
-    <select className="bg-gray-200 border rounded-l-lg">
-      <option value="all">All</option>
-      {/* Add more options as needed */}
-    </select>
-    <div className="relative flex-grow">
+    <div className="flex items-center bg-gray-100 rounded-3xl shadow-md w-full">
+      <select className="bg-gray-300 border rounded-l-lg ">
+        <option value="all">All</option>
+        {/* Add more options as needed */}
+      </select>
       <input
         type="text"
-        className="w-full border-5 bg-blue-200 pr-10"
+        className="w-full h-6 border-5 rounded-l-none bg-blue-200 px-2 py-2 pr-2"
         placeholder="Search..."
       />
-      <button
-        className="absolute inset-y-0 right-0 flex items-center px-2 text-categrycolor"
-    
-      >
-        clear
+      <button className=" px-2  bg-[#ba3030] text-white rounded-r-lg">
+        FAZIT
       </button>
     </div>
-    
-    <button className="px-2 bg-[#ba3030] text-white rounded-r-lg">
-      FAZIT
-    </button>
-  </div>
-  
   );
 };
 
