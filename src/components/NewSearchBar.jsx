c8e9a89b856d1e7cf502d51f2bfc6b8dbd348dfb
Syed Issam Bukhari <76740289+syedissambukhari@users.noreply.github.com>
2024-08-17 00:59:36 +0500

    Add files via upload

---
+++ src/components/NewSearchBar.jsx
@@ -0,0 +1,24 @@
+
+// src/components/newSearchBar.js
+import React from "react";
+
+const NewSearchBar = () => {
+  return (
+    <div className="flex items-center bg-gray-100 rounded-3xl shadow-md w-full">
+      <select className="bg-gray-200 border rounded-l-lg ">
+        <option value="all">All</option>
+        {/* Add more options as needed */}
+      </select>
+      <input
+        type="text"
+        className="flex-grow border-5 w-80 bg-blue-200"
+        placeholder="Returned Data"
+      />
+      <button className=" px-2  bg-[#ba3030] text-white rounded-r-lg">
+        FAZIT
+      </button>
+    </div>
+  );
+};
+
+export default NewSearchBar;
