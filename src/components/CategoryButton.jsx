92db4124bf3679149eadca13a8e4258ddadebd4f
Syed Issam Bukhari <76740289+syedissambukhari@users.noreply.github.com>
2024-08-16 20:07:51 +0500

    Update CategoryButton.jsx

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const CategoryButton = ({ label, count, icon, selected, onClick }) => {
   return (
     <div
-      className="flex flex-col items-center  w-auto px-4 pl-2 cursor-pointer"
+      className="flex flex-col items-center w-auto px-4 py-2 cursor-pointer transition-transform transform hover:scale-105"
       onClick={onClick}
     >
       <span
-        className={`text-[10px] ${
-          selected ? "text-red-700 " : "text-gray-700"
-        }`}
+        className={`text-xs ${selected ? "text-red-700" : "text-gray-700"}`}
       >
         {label}
       </span>
-      <div className=" my-[10px] ml-[10px] mr-[5px] bg-white ">
-        <img src={icon} alt={label} className="" />
+      <div className="my-[10px]  flex items-center justify-center">
+        <img
+          src={icon}
+          alt={label}
+          className=" object-contain" 
+          style={{ mixBlendMode: "multiply" }} 
+        />
       </div>
-
       <span
-        className={`text-sm font-  ${
-          selected ? "text-red-700 " : "text-gray-700"
-        }`}
-        style={{ fontFamily: "arial" }}
+        className={`text-sm ${selected ? "text-red-700" : "text-gray-700"}`}
+        style={{ fontFamily: "Arial" }}
       >
         {count}
       </span>
