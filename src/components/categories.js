6cf0d4e57ef3d490aa40f15cc2bbb1ffd9a6bc8f
Zainabkhan9118 <zainabkhan3473@gmail.com>
2024-08-19 13:44:04 +0500

    images added

--- src/components/categories.js
+++ src/components/categories.js
@@ -5,35 +5,46 @@ import c4 from "../assets/icon-sites.png";
 import c5 from "../assets/icon-headlines.png";
 import c6 from "../assets/icon-sports.png";
 import c7 from "../assets/icon-videos.png";
+import c8 from "../assets/country_icons/united_states_of_america.png";
+import c9 from "../assets/country_icons/germany.png";
+import c10 from "../assets/country_icons/united_kingdom.png";
+import c11 from "../assets/country_icons/france.png";
+import c12 from "../assets/country_icons/comoros.png";
 export const countryCategories = [
   {
     label: "All",
     count: 401834,
     icon: c1,
-    selected: false,
+    selected: true,
   },
   {
     label: "UnitedStates",
     count: 173620,
-    icon: c2,
-    selected: true,
+    icon: c8,
+    selected: false,
   },
   {
     label: "Germany",
     count: 401834,
-    icon: c3,
+    icon: c9,
     selected: false,
   },
   {
     label: "UnitedKingdom",
     count: 173620,
-    icon: c4,
+    icon: c10,
+    selected: false,
+  },
+  {
+    label: "Comoros",
+    count: 28459,
+    icon: c12,
     selected: false,
   },
   {
     label: "France",
     count: 401834,
-    icon: c5,
+    icon: c11,
     selected: false,
   },
   {
@@ -60,25 +71,25 @@ export const dataCategories = [
   },
   {
     label: "Video",
-    count: 173620,
+    count: 16482,
     icon: c5,
     selected: false,
   },
   {
     label: "Headline",
-    count: 48501,
+    count: 15269,
     icon: c7,
     selected: false,
   },
   {
-    label: "Site",
-    count: 173620,
+    label: "Sites",
+    count: 11574,
     icon: c1,
     selected: false,
   },
   {
     label: "Exit Search",
-    count: 48501,
+    count: 9852,
     icon: c4,
     selected: false,
   },
@@ -87,39 +98,45 @@ export const dataCategories = [
 
 export const industryCategories = [
   {
-    label: "Pages",
-    count: 173620,
-    icon: c7,
-    selected: true,
-  },
-  {
-    label: "Sports",
-    count: 123472,
-    icon: c5,
-    selected: false,
-  },
-  {
-    label: "New",
-    count: 173620,
-    icon: c3,
-    selected: false,
-  },
-  {
-    label: "Business",
-    count: 123472,
-    icon: c1,
-    selected: false,
-  },
-  {
-    label: "Celebration",
+    label: "News",
     count: 173620,
     icon: c2,
     selected: true,
   },
   {
-    label: "TV&Movies",
+    label: "Sports",
     count: 123472,
-    icon: c4,
+    icon: c6,
+    selected: false,
+  },
+  {
+    label: "Health",
+    count: 24879,
+    icon: c6,
+    selected: false,
+  },
+  {
+    label: "Business",
+    count: 8317,
+    icon: c6,
+    selected: false,
+  },
+  {
+    label: "Celebrities",
+    count: 2874,
+    icon: c6,
+    selected: false,
+  },
+  {
+    label: "TV&Movies",
+    count: 845,
+    icon: c6,
+    selected: false,
+  },
+  {
+    label: "More",
+    count: 14983,
+    icon: c6,
     selected: false,
   },
   // Add more industries
@@ -130,7 +147,7 @@ export const languageCategories = [
     label: "English",
     count: 401834,
     icon: c2,
-    selected: true,
+    selected: false,
   },
 ];
 
